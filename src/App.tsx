a2d3416fd8b50890bdcf523d19b7307cc2de14d6
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-24 22:05:54 -0600

    feat: Implemented data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react'
+import { Nft, Owner } from './types'
+import nftData from './data/nft'
+
 function App() {
-	return <></>
+	const [nft, setNft] = useState<Nft>()
+
+	useEffect(() => {
+		setNft(nftData)
+	}, [])
+
+	if (!nft) return null
+
+	const owner: Owner = nft.owner
+	const ownerMessage = owner.author ? 'Creation of' : 'Property of'
+
+	return (
+		<>
+			<section className="card">
+				<img src={nft.image} alt={`${nft.title} image`} />
+				<article className="content">
+					<h1 className="title">{nft.title}</h1>
+					<p className="description">{nft.description}</p>
+					<div className="information"></div>
+					<div className="divider"></div>
+					<div className="information owner">
+						<img src={owner.image} alt={owner.name} />
+						<p>
+							{ownerMessage} <span className="important">{owner.name}</span>
+						</p>
+					</div>
+				</article>
+			</section>
+		</>
+	)
 }
 
 export default App
